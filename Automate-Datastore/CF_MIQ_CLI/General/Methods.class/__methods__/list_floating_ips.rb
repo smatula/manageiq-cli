79053bc799e0a3422af901ad0251afbbac71592e
Stephen Matula <smatula@redhat.com>
2018-03-16 09:40:10 -0400

    testing

--- Automate-Datastore/CF_MIQ_CLI/General/Methods.class/__methods__/list_floating_ips.rb
+++ Automate-Datastore/CF_MIQ_CLI/General/Methods.class/__methods__/list_floating_ips.rb
@@ -87,8 +87,8 @@ begin
   ip_list = {}
   ips.each { |f| ip_list[f.address] = { :id => f.id,
                                      :fixed_ip_address => f.fixed_ip_address,
-                                     :network_provider => f.ext_management_system,
-                                     :ext_floating_network => $evm.vmdb(:cloud_network).find_by_id(f.cloud_network_id).name
+                                     :network_provider => f.ext_management_system
+                                     #:ext_floating_network => $evm.vmdb(:cloud_network).find_by_id(f.cloud_network_id).name
                                      #:instance => f.vm_id.nil?? "" : $evm.vmdb(:vm).find_by_id(f.vm_id).name
                                    }} if ips
 
