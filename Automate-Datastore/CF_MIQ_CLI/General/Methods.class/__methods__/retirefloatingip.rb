e4802cffd5412b8686748d68971becea49c3d215
Stephen Matula <smatula@redhat.com>
2018-03-20 11:46:19 -0400

    provider

--- Automate-Datastore/CF_MIQ_CLI/General/Methods.class/__methods__/retirefloatingip.rb
+++ Automate-Datastore/CF_MIQ_CLI/General/Methods.class/__methods__/retirefloatingip.rb
@@ -90,7 +90,7 @@ begin
   dump_root
   # Get EMS ID
   ems_id = $evm.object['cloud_provider_id'] if $evm.object['cloud_provider_id']
-  ems_id = $evm.root(:external_management_system).find_by_name($evm.object['cloud_provider']).id if $evm.object['cloud_provider']
+  ems_id = $evm.vmdb(:provider).find_by_name($evm.object['cloud_provider']).id if $evm.object['cloud_provider']
   log(:info, "Provider emd_id: #{ems_id}")
 
   # Get VM by ID. 
