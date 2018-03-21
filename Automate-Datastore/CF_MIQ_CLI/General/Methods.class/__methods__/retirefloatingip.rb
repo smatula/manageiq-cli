a79616609510ad74e30b8e8511b01dd1dfa88c1e
Stephen Matula <smatula@redhat.com>
2018-03-21 08:18:29 -0400

    format for ruby

--- Automate-Datastore/CF_MIQ_CLI/General/Methods.class/__methods__/retirefloatingip.rb
+++ Automate-Datastore/CF_MIQ_CLI/General/Methods.class/__methods__/retirefloatingip.rb
@@ -100,10 +100,10 @@ begin
   if $evm.object['vm_name']
     # Build Where string
     where_str = "name = " + $evm.object['vm_name']
-    where_str = where_str + " and ems_id = " + ems_id if ems_id
-    where_str = where_str + " and cloud_network_id = " + $evm.object['cloud_network_id'] if $evm.object['cloud_network_id']
-    where_str = where_str + " and cloud_tenant_id = " + $evm.object['cloud_tenant_id'] if $evm.object['cloud_tenant_id']
-    where_str = where_str + " and cloud_network_id = " + $evm.object['cloud_network_id'] if $evm.object['cloud_network_id']
+    where_str = where_str + " and ems_id = #{ems_id}" if ems_id
+    where_str = where_str + " and cloud_network_id = #{$evm.object['cloud_network_id']}" if $evm.object['cloud_network_id']
+    where_str = where_str + " and cloud_tenant_id = #{$evm.object['cloud_tenant_id']}" if $evm.object['cloud_tenant_id']
+    where_str = where_str + " and cloud_network_id = #{$evm.object['cloud_network_id']}" if $evm.object['cloud_network_id']
 
     vm_list = []
     vm_list = $evm.vmdb(:vm).where([where_str])
